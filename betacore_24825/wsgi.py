09202f27073cf9ee48c6c6308a9a0a664ba7ef9a
Crowdbotics <team@crowdbotics.com>
2021-03-03 14:13:22 +0000

    Update models

--- betacore_24825/wsgi.py
+++ betacore_24825/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'betacore_24825.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betacore_24825.settings")
 
 application = get_wsgi_application()
